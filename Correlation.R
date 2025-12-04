e7dd6bb019181e836224cc18e2255c060c0fc35c
Ahmad Khalil <ak24akr@herts.ac.uk>
2025-12-04 00:07:33 +0000

    change column names which are possible to get better understanding of data

---
+++ Correlation.R
@@ -0,0 +1,13 @@
+library(readr)
+df <- read.csv("DGPT24.csv")
+
+class(df)
+summary(df)
+print(colnames(df))
+      
+#changing names of Columns which are possible for better understanding
+colnames(df)[5]<-"parked_%"
+colnames(df)[8]<-"fairway_%"
+colnames(df)[9]<-"scramble_%"
+
+print(colnames(df))
