d26550204ccdfee59288eacde7803939c9806327
Ahmad Khalil <ak24akr@herts.ac.uk>
2025-12-07 19:49:55 +0000

    Checking the types of our Coulmn (i.e vector,numeric or integer etc) which are used in our Research Question. As our RQ is on Correlation so we can only use numeric and integer type.

--- Correlation.R
+++ Correlation.R
@@ -3,7 +3,7 @@ df <- read.csv("DGPT24.csv")
 
 class(df)
 summary(df)
-print(colnames(df))
+#print(colnames(df))
       
 #changing names of Columns which are possible for better understanding
 colnames(df)[5]<-"parked_%"
@@ -11,3 +11,8 @@ colnames(df)[8]<-"fairway_%"
 colnames(df)[9]<-"scramble_%"
 
 print(colnames(df))
+
+#Checking the types of Columns especially used in our Research Question
+class(df$birdie_avg)
+class(df$C1R)
+class(df$total_earnings)
