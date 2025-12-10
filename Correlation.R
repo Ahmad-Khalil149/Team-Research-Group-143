22d8f5e63b09d32d104b2381e5ec40f8578b040d
Ahmad Khalil <ak24akr@herts.ac.uk>
2025-12-10 00:54:47 +0000

    checking the data is normal or not. In our case one variacle C1X is normal But total_earning is not normal. So, we use Spearmen method.

--- Correlation.R
+++ Correlation.R
@@ -1,30 +1,32 @@
 library(readr)
-df <- read.csv("DGPT24.csv")
+DGPT24 <- read.csv("DGPT24.csv")
 
-class(df)
-summary(df)
-#print(colnames(df))
+class(DGPT24)
+summary(DGPT24)
+#print(colnames(DGPT24))
       
 #changing names of Columns which are possible for better understanding
-colnames(df)[5]<-"parked_%"
-colnames(df)[8]<-"fairway_%"
-colnames(df)[9]<-"scramble_%"
+colnames(DGPT24)[5]<-"parked_%"
+colnames(DGPT24)[8]<-"fairway_%"
+colnames(DGPT24)[9]<-"scramble_%"
 
-print(colnames(df))
+print(colnames(DGPT24))
 
 #Checking the types of Columns especially used in our Research Question
 
-class(df$birdie_avg)
+class(DGPT24$birdie_avg)
 #as first Column C1X which is numeric and Our 2nd column total_earnings are integer type
-class(df$C1X)
-class(df$total_earnings)
+class(DGPT24$C1X)
+class(DGPT24$total_earnings)
 
 
 #Now, removing the unrealistic values from Column C1X and total_earnings.
-df <- df[df$C1X >= 0 & df$C1X <= 100, ]
-df <- df[df$total_earning > 0, ]
+DGPT24 <- DGPT24[DGPT24$C1X >= 0 & DGPT24$C1X <= 100, ]
+DGPT24 <- DGPT24[DGPT24$total_earning > 0, ]
 
-
+#Now check that the data is normal or not
+hist(DGPT24$C1X, main="Histogram of C1X", xlab="C1X", col="green", border="black")
+hist(DGPT24$total_earnings, main="Histogram of Total Earnings", xlab="Total Earnings", col="yellow", border="black")
 
 
 
